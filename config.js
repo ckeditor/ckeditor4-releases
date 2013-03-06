be3ccb9f77c9b10965dda7ca7d3a7fb38e5867e2
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2013-03-06 15:49:38 +0100

    Added CKEditor 4.1rc standard.

--- config.js
+++ config.js
@@ -29,4 +29,10 @@ CKEDITOR.editorConfig = function( config ) {
 	// Remove some buttons, provided by the standard plugins, which we don't
 	// need to have in the Standard(s) toolbar.
 	config.removeButtons = 'Underline,Subscript,Superscript';
+
+	// Se the most common block elements.
+	config.format_tags = 'p;h1;h2;h3;pre';
+
+	// Make dialogs simpler.
+	config.removeDialogTabs = 'image:advanced;link:advanced';
 };
