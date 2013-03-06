2f3abb772d3184f485af8444fe780e86e4e3aeb4
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2013-03-06 16:12:03 +0100

    Added CKEditor 4.0.2 basic.

--- samples/sample.js
+++ samples/sample.js
@@ -1,5 +1,33 @@
-﻿/*
- Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
- For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
-(function(){CKEDITOR.on("instanceReady",function(b){var b=b.editor,a=CKEDITOR.document.$.getElementsByName("ckeditor-sample-required-plugins"),a=a.length?CKEDITOR.dom.element.get(a[0]).getAttribute("content").split(","):[],c=[];if(a.length){for(var d=0;d<a.length;d++)b.plugins[a[d]]||c.push("<code>"+a[d]+"</code>");c.length&&CKEDITOR.dom.element.createFromHtml('<div class="warning"><span>To fully experience this demo, the '+c.join(", ")+" plugin"+(1<c.length?"s are":" is")+" required.</span></div>").insertBefore(b.container)}})})();
+﻿/**
+ * Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+// Tool scripts for the sample pages.
+// This file can be ignored and is not required to make use of CKEditor.
+
+(function() {
+	// Check for sample compliance.
+	CKEDITOR.on( 'instanceReady', function( ev ) {
+		var editor = ev.editor,
+			meta = CKEDITOR.document.$.getElementsByName( 'ckeditor-sample-required-plugins' ),
+			requires = meta.length ? CKEDITOR.dom.element.get( meta[ 0 ] ).getAttribute( 'content' ).split( ',' ) : [],
+			missing = [];
+
+		if ( requires.length ) {
+			for ( var i = 0; i < requires.length; i++ ) {
+				if ( !editor.plugins[ requires[ i ] ] )
+					missing.push( '<code>' + requires[ i ] + '</code>' );
+			}
+
+			if ( missing.length ) {
+				var warn = CKEDITOR.dom.element.createFromHtml(
+					'<div class="warning">' +
+						'<span>To fully experience this demo, the ' + missing.join( ', ' ) + ' plugin' + ( missing.length > 1 ? 's are' : ' is' ) + ' required.</span>' +
+					'</div>'
+				);
+				warn.insertBefore( editor.container );
+			}
+		}
+	});
+})();
