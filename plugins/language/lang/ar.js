b39eebd5f865fe33e34e33db9a33512655e6e656
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2014-03-25 15:33:54 +0100

    Added CKEditor 4.3.4 standard-all.

--- plugins/language/lang/ar.js
+++ plugins/language/lang/ar.js
@@ -2,4 +2,4 @@
  Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
  For licensing, see LICENSE.md or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang("language","pt",{button:"Definir Idioma",remove:"Remover Idioma"});
+CKEDITOR.plugins.setLang("language","ar",{button:"Set language",remove:"Remove language"});
