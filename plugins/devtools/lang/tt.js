7d88a6ab0efe61890edb131420a53dc8c7568a74
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2014-04-24 16:05:16 +0200

    Added CKEditor 4.4.0 standard-all.

--- plugins/devtools/lang/tt.js
+++ plugins/devtools/lang/tt.js
@@ -2,4 +2,4 @@
  Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
  For licensing, see LICENSE.md or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang("devtools","tt",{title:"Элемент тасвирламасы",dialogName:"Dialog window name",tabName:"Өстәмә бит исеме",elementId:"Element ID",elementType:"Элемент төре"});
+CKEDITOR.plugins.setLang("devtools","tt",{title:"Элемент тасвирламасы",dialogName:"Диалог тәрәзәсе исеме",tabName:"Өстәмә бит исеме",elementId:"Элемент идентификаторы",elementType:"Элемент төре"});
