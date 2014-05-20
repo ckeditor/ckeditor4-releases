86ddb21e08c748eabe69ab00ede0d7a65b1298c7
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2014-05-20 15:39:45 +0200

    Added CKEditor 4.4.1 standard-all.

--- plugins/codesnippet/lang/eo.js
+++ plugins/codesnippet/lang/eo.js
@@ -2,4 +2,4 @@
  Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
  For licensing, see LICENSE.md or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang("codesnippet","eo",{button:"Enmeti kodaĵon",codeContents:"Code content",emptySnippetError:"A code snippet cannot be empty.",language:"Language",title:"Code snippet"});
+CKEDITOR.plugins.setLang("codesnippet","eo",{button:"Enmeti kodaĵeron",codeContents:"Kodenhavo",emptySnippetError:"Kodaĵero ne povas esti malplena.",language:"Lingvo",title:"Kodaĵero"});
