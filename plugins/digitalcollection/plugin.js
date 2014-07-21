09098127fa9148256d2ed1bbfdf104285c90c9da
Edwin Guzman <edwin.gzmn@gmail.com>
2014-07-21 12:52:15 -0400

    Removing the context menu - too many bugs

--- plugins/digitalcollection/plugin.js
+++ plugins/digitalcollection/plugin.js
@@ -18,22 +18,22 @@ CKEDITOR.plugins.add('digitalcollection', {
       icon: this.path + 'images/digitalcollection.png'
     });
 
-    if (editor.contextMenu) {
-      editor.addMenuGroup('digitalCollectionGroup');
-      editor.addMenuItem('digitalCollectionItem', {
-        label: 'Edit Digital Collections Image',
-        icon: this.path + 'images/digitalcollection.png',
-        command: 'digitalcollectionDialog',
-        group: 'digitalCollectionGroup'
-      });
-      editor.contextMenu.addListener(function (element) {
-        var catalogElement = element.getAscendant('div', true);
-        if (catalogElement &&
-            catalogElement.getAttribute('class').indexOf('digcol-image') !== 0) {
-          return { digitalCollectionItem: CKEDITOR.TRISTATE_OFF };
-        }
-      });
-    }
+    // if (editor.contextMenu) {
+    //   editor.addMenuGroup('digitalCollectionGroup');
+    //   editor.addMenuItem('digitalCollectionItem', {
+    //     label: 'Edit Digital Collections Image',
+    //     icon: this.path + 'images/digitalcollection.png',
+    //     command: 'digitalcollectionDialog',
+    //     group: 'digitalCollectionGroup'
+    //   });
+    //   editor.contextMenu.addListener(function (element) {
+    //     var catalogElement = element.getAscendant('div', true);
+    //     if (catalogElement &&
+    //         catalogElement.getAttribute('class').indexOf('digcol-image') !== 0) {
+    //       return { digitalCollectionItem: CKEDITOR.TRISTATE_OFF };
+    //     }
+    //   });
+    // }
 
     editor.widgets.add('digitalcollection', {
       dialog: 'digitalcollectionDialog',
