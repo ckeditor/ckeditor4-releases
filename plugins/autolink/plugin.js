51dd629562a3b7538b9a1ed222a23d77b9a4e10b
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2015-08-04 16:58:12 +0200

    Added CKEditor 4.5.2 standard-all.

--- plugins/autolink/plugin.js
+++ plugins/autolink/plugin.js
@@ -2,4 +2,4 @@
  Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
  For licensing, see LICENSE.md or http://ckeditor.com/license
 */
-CKEDITOR.plugins.add("autolink",{requires:"clipboard",init:function(c){c.on("paste",function(a){var b=a.data.dataValue;a.data.dataTransfer.getTransferType(c)!=CKEDITOR.DATA_TRANSFER_INTERNAL&&!(-1<b.indexOf("<"))&&(b=b.replace(/^(https?|ftp):\/\/(-\.)?([^\s\/?\.#-]+\.?)+(\/[^\s]*)?[^\s\.,]$/ig,'<a href="$&">$&</a>'),b!=a.data.dataValue&&(a.data.type="html"),a.data.dataValue=b)})}});
+(function(){var d=/^(https?|ftp):\/\/(-\.)?([^\s\/?\.#-]+\.?)+(\/[^\s]*)?[^\s\.,]$/ig,e=/"/g;CKEDITOR.plugins.add("autolink",{requires:"clipboard",init:function(c){c.on("paste",function(b){var a=b.data.dataValue;b.data.dataTransfer.getTransferType(c)!=CKEDITOR.DATA_TRANSFER_INTERNAL&&!(-1<a.indexOf("<"))&&(a=a.replace(d,'<a href="'+a.replace(e,"%22")+'">$&</a>'),a!=b.data.dataValue&&(b.data.type="html"),b.data.dataValue=a)})}})})();
