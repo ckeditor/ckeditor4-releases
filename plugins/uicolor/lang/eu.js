4b0678ff6f9e643f8361727f3a1325e4928b3d51
Piotrek Koszuliński <pkoszulinski@gmail.com>
2015-11-12 20:22:59 +0100

    Added CKEditor 4.5.5 standard-all.

--- plugins/uicolor/lang/eu.js
+++ plugins/uicolor/lang/eu.js
@@ -2,4 +2,4 @@
  Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
  For licensing, see LICENSE.md or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang("uicolor","eu",{title:"EI Kolore Hautatzailea",preview:"Zuzeneko aurreikuspena",config:"Itsatsi karaktere kate hau zure config.js fitxategian.",predefined:"Aurredefinitutako kolore multzoak"});
+CKEDITOR.plugins.setLang("uicolor","eu",{title:"EI kolore-hautatzailea",preview:"Zuzeneko aurrebista",config:"Itsatsi kate hau zure config.js fitxategian",predefined:"Aurrez definitutako kolore multzoak"});
--- plugins/uploadwidget/lang/ug.js
+++ plugins/uploadwidget/lang/ug.js
@@ -2,4 +2,4 @@
  Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
  For licensing, see LICENSE.md or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang("uploadwidget","ug",{abort:"يۈكلەشنى ئىشلەتكۈچى ئۈزۈۋەتتى.",doneOne:"File successfully uploaded.",doneMany:"Successfully uploaded %1 files.",uploadOne:"Uploading file ({percentage}%)...",uploadMany:"Uploading files, {current} of {max} done ({percentage}%)..."});
+CKEDITOR.plugins.setLang("uploadwidget","ug",{abort:"يۈكلەشنى ئىشلەتكۈچى ئۈزۈۋەتتى.",doneOne:"ھۆججەت مۇۋەپپەقىيەتلىك يۈكلەندى.",doneMany:"مۇۋەپپەقىيەتلىك ھالدا %1 ھۆججەت يۈكلەندى.",uploadOne:"Uploading file ({percentage}%)...",uploadMany:"Uploading files, {current} of {max} done ({percentage}%)..."});
