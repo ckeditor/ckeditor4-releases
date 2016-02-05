f49b62d4d7fb72b542a56ec4eba9157b57254e17
Andrew Dowds <adowds@gmail.com>
2016-02-05 02:57:04 +0000

    moved totally customized version in

--- plugins/placeholder/dialogs/placeholder.js
+++ plugins/placeholder/dialogs/placeholder.js
@@ -1,5 +1,65 @@
-﻿/*
- Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
- For licensing, see LICENSE.md or http://ckeditor.com/license
-*/
-CKEDITOR.dialog.add("placeholder",function(a){var b=a.lang.placeholder,a=a.lang.common.generalTab;return{title:b.title,minWidth:300,minHeight:80,contents:[{id:"info",label:a,title:a,elements:[{id:"name",type:"text",style:"width: 100%;",label:b.name,"default":"",required:!0,validate:CKEDITOR.dialog.validate.regex(/^[^\[\]<>]+$/,b.invalidName),setup:function(a){this.setValue(a.data.name)},commit:function(a){a.setData("name",this.getValue())}}]}]}});
+﻿/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+(function() {
+	function placeholderDialog( editor, isEdit ) {
+
+		var lang = editor.lang.placeholder,
+			generalLabel = editor.lang.common.generalTab;
+		return {
+			title: 'Receipt Merge Field',//lang.title,
+			minWidth: 300,
+			minHeight: 80,
+			contents: [
+				{
+				id: 'info',
+				label: generalLabel,
+				title: generalLabel,
+				elements: [
+					{
+					  id: 'text',
+					  type: 'select',
+            items:[['First Name'],['Last Name'],['Donation Amount'],['Donation Date'],['Project Title'],['Perk Name'],['Perk FMV'],['Tax Deductible Amount']],
+            default:['First Name'],
+
+					//type: 'text',
+					style: 'width: 100%;',
+					label: 'Choose a value to merge in your receipt.',//lang.text,
+					'default': '',
+					required: true,
+					validate: CKEDITOR.dialog.validate.notEmpty( lang.textMissing ),
+					setup: function( element ) {
+						if ( isEdit )
+							this.setValue( element.getText().slice( 2, -2 ) );
+					},
+					commit: function( element ) {
+						var text = '[[' + this.getValue() + ']]';
+						// The placeholder must be recreated.
+						CKEDITOR.plugins.placeholder.createPlaceholder( editor, element, text );
+					}
+				}
+				]
+			}
+			],
+			onShow: function() {
+				if ( isEdit )
+					this._element = CKEDITOR.plugins.placeholder.getSelectedPlaceHolder( editor );
+
+				this.setupContent( this._element );
+			},
+			onOk: function() {
+				this.commitContent( this._element );
+				delete this._element;
+			}
+		};
+	}
+
+	CKEDITOR.dialog.add( 'createplaceholder', function( editor ) {
+		return placeholderDialog( editor );
+	});
+	CKEDITOR.dialog.add( 'editplaceholder', function( editor ) {
+		return placeholderDialog( editor, 1 );
+	});
+})();
