3a9c1a1d3b2abde77f3ec3680b00cca500111ecc
Krzysztof Krztoń <k.krzton@cksource.com>
2020-11-09 15:11:16 +0100

    Added CKEditor 4.15.1 standard.

--- build-config.js
+++ build-config.js
@@ -13,10 +13,10 @@
  * (1) https://ckeditor.com/cke4/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) https://ckeditor.com/cke4/builder/cfbaec8ebebc17d4b1bea88ab3d948a0
+ * (2) https://ckeditor.com/cke4/builder/4c616e991e9c7b31849de271ab87cf37
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) https://ckeditor.com/cke4/builder/download/cfbaec8ebebc17d4b1bea88ab3d948a0
+ * (3) https://ckeditor.com/cke4/builder/download/4c616e991e9c7b31849de271ab87cf37
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -39,6 +39,7 @@ var CKBUILDER_CONFIG = {
 		'.jshintrc',
 		'.mailmap',
 		'.npm',
+		'.nvmrc',
 		'.travis.yml',
 		'bender-err.log',
 		'bender-out.log',
