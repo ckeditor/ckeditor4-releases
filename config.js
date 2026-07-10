43635b2a9ecff77a6445d9b6ed92e51b4a410424
Jacek Bogdanski <jacek.bogdanski@icloud.com>
2026-07-10 15:56:59 +0200

    Added CKEditor 4.25.2 standard.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright (c) 2003-2025, CKSource Holding sp. z o.o. All rights reserved.
+ * @license Copyright (c) 2003-2026, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
